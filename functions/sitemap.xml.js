c3a1886e98a04a318c0c995277b63bba6ab8085c
kevin W <wangkanghk@gmail.com>
2026-07-07 23:28:13 +0800

    Add sitemap and robots endpoints

---
+++ functions/sitemap.xml.js
@@ -0,0 +1,43 @@
+const lastmod = "2026-07-07";
+
+function escapeXml(value) {
+  return value
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&apos;");
+}
+
+export function onRequestGet({ request }) {
+  const { origin } = new URL(request.url);
+  const urls = [
+    {
+      loc: `${origin}/`,
+      priority: "1.0",
+      changefreq: "weekly",
+    },
+  ];
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (url) => `  <url>
+    <loc>${escapeXml(url.loc)}</loc>
+    <lastmod>${lastmod}</lastmod>
+    <changefreq>${url.changefreq}</changefreq>
+    <priority>${url.priority}</priority>
+  </url>`
+  )
+  .join("\n")}
+</urlset>
+`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=3600",
+    },
+  });
+}
